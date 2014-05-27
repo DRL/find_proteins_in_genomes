d7a5238dbecdf7660789f58e28439fff95ccf89a
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2014-05-27 10:14:48 +0100

    debug

--- parse_tblastn.py
+++ parse_tblastn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, os
 import collections
 # qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore -> std
 # GPLIN_000706300 contig_3262     77.78   81      0       1       55      117     4938    4696    2e-33    126
@@ -24,23 +24,24 @@ def parse_blast_to_dict(filename):
 				strand = '-'   
 			if query in blast_dict:
 				if subject in blast_dict[query]:
-					if strand == '+':
-						if blast_dict[query][subject][2] < send:
-							blast_dict[query][subject][2] = send
-						if blast_dict[query][subject][1] > sstart:
-							blast_dict[query][subject][1] = sstart
-					else:
-						if blast_dict[query][subject][2] > send:
-							blast_dict[query][subject][2] = send
-						if blast_dict[query][subject][1] < sstart:
-							blast_dict[query][subject][1] = sstart
+					if strand == blast_dict[query][subject][0]:
+						if strand == '+':
+							if blast_dict[query][subject][2] < send:
+								blast_dict[query][subject][2] = send
+							if blast_dict[query][subject][1] > sstart:
+								blast_dict[query][subject][1] = sstart
+						else:
+							if blast_dict[query][subject][2] > send:
+								blast_dict[query][subject][2] = send
+							if blast_dict[query][subject][1] < sstart:
+								blast_dict[query][subject][1] = sstart
 				else:
 					blast_dict[query][subject] = [strand, sstart, send]
 			else:  
 				blast_dict[query][subject] = [strand, sstart, send]
 	return blast_dict
 
-def parse_assembly_to_dict(filename):
+def parse_fasta_to_dict(filename):
 	assembly_dict = {}
 	with open(filename) as fh:
 		header = ''
@@ -49,10 +50,11 @@ def parse_assembly_to_dict(filename):
 			line = line.rstrip("\n")
 			if line.startswith(">"):
 				header = line.lstrip(">")
-				assembly_dict[header] = seq
+				assembly_dict[header] = ''
+				seq = ''
         	else:
-        		assembly_dict[header] += seq
-        assembly_dict[header] = seq
+        		seq += line
+        assembly_dict[header] = line
 	return assembly_dict
 
 def print_blast_dict(blast_dict):
@@ -62,15 +64,40 @@ def print_blast_dict(blast_dict):
 			strand = blast_dict[query][subject][0]
 			sstart = blast_dict[query][subject][1]
 			send = blast_dict[query][subject][2]
-			print ("\t" + subject + "\t" + strand + "\t" + str(sstart) + "\t" + str(send))
+			#print ("\t" + subject + "\t" + strand + "\t" + str(sstart) + "\t" + str(send))
+			if strand == '+':
+				print ("\t" + subject + "\t" + str(send - sstart))
+			else:
+				print ("\t" + subject + "\t" + str(sstart - send))
 
+def print_protein_dict(protein_dict):
+	for header in protein_dict:
+		filename = os.path.dirname(protein_file) + '/' + header + '.fa'
+		with open(filename) as fh:
+			fh.write(">" + header + "\n" + protein_dict[header] + "\n")
+
+def print_regions(assembly_file, assembly_dict, blast_dict, region):
+	for query in blast_dict:
+		for subject in blast_dict[query]:
+			print subject
+			sequence = assembly_dict[subject]
+			print sequence
+			sequence_region = sequence[blast_dict[query][subject][1]:blast_dict[query][subject][2]]
+			print sequence_region
+			#filename = os.path.dirname(assembly_file) + '/' + assembly_file + '.' + query + '.' + subject + '.fa'
+			filename = './' + assembly_file + '.' + query + '.' + subject + '.fa'
+			fh = open(filename, "w")
+			fh.write(">" + assembly_file + '.' + query + '.' + subject + '_' + str(REGION) + "\n" + sequence_region + "\n")
 
 if __name__ == "__main__":
 	blast_file = sys.argv[1]
-	assembly_file = sys.argv[2]
-	region = sys.argv[3]
-
+	#protein_file = sys.argv[2]
+	assembly_file= sys.argv[2]
+	REGION = 1000
 	blast_dict = parse_blast_to_dict(blast_file)
-	assembly_dict = parse_assembly_to_dict(assembly_file)
+	#protein_dict = parse_fasta_to_dict(protein_file)
+	#print_protein_dict(protein_dict)
+	assembly_dict = parse_fasta_to_dict(assembly_file)
+	print assembly_dict
+	print_regions(assembly_file, assembly_dict, blast_dict, REGION)
 	print_blast_dict(blast_dict)
-	# get_regions_from_assembly(assembly_dict, region)
